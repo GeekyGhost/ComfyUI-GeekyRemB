56415e3060a70ca51bfe3e376bc17a0e28e67065
GeekyGhost <111990299+GeekyGhost@users.noreply.github.com>
2024-08-29 09:40:00 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,4 @@
-from .GeekyRembv2 import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .scripts.GeekyRembv2 import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+# Optionally, you can also add:
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
